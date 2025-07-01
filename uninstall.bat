365ddc60d1c8822f6e555f8025e76eda8c70cf3d
stikarb <Stikarb@yandex.ru>
2025-07-01 17:58:30 +0300

    unInstaller fo Win

--- uninstall.bat
+++ uninstall.bat
@@ -1,19 +1,24 @@
 @echo off
-set /p choice="Вы хотите удалить только временные файлы (1) или весь проект (2)? "
+set /p choice="Выберите действие [1-2]: 
+1. Удалить только временные файлы
+2. Удалить весь проект
+"
 
 if "%choice%"=="1" (
-    echo Удаление временных файлов...
-    rmdir /s /q venv
-    rmdir /s /q dist
-    rmdir /s /q build
+    echo Очистка временных файлов...
+    rmdir /s /q venv 2>nul
+    rmdir /s /q dist 2>nul
+    rmdir /s /q build 2>nul
     del news_parser.exe 2>nul
     del game_news.html 2>nul
     del news_parser.spec 2>nul
+    echo Временные файлы удалены
 ) else if "%choice%"=="2" (
-    echo Удаление всего проекта...
-    set "folder=%cd%"
+    echo Полное удаление проекта...
+    set "project_path=%cd%"
     cd ..
-    rmdir /s /q "%folder%"
+    rmdir /s /q "%project_path%"
+    echo Проект удален
 ) else (
-    echo Неверный выбор.
+    echo Неверный выбор
 )
